bc67417a1f7726b5abaa3a9f1b22af587fb36547
JeffreyWa <wang_jun_zhan@yahoo.com>
2023-09-24 03:15:25 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
-source /home/codespace/venv/bin/activate
+sudo apt-get update && apt-get -y install --no-install-recommends \
+   python3.11-venv
+USER="codespace"
+VENV_PATH="/home/${USER}/venv"
+su $USER -c "/usr/bin/python3 -m venv /home/${USER}/venv" \
+source /home/vocode/venv/bin/activate
 #append it to bash so every shell launches with it 
-echo 'source /home/codespace/venv/bin/activate' >> ~/.bashrc
-make install-tensorflow
+echo 'source /home/vscode/venv/bin/activate' >> ~/.bashrc
+su $USER -c "pip3 --disable-pip-version-check --no-cache-dir install -r requirements.txt"
