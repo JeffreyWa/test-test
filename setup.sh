43d694e575927f7ef3094953bfa12731c83aca2c
JeffreyWa <wang_jun_zhan@yahoo.com>
2023-09-24 02:13:03 +0000

    update

--- setup.sh
+++ setup.sh
@@ -7,6 +7,6 @@ set -x
 #su $USER -c "/usr/bin/python3 -m venv /home/${USER}/venv"
 echo 'source /home/vscode/venv/bin/activate' >> ~/.bashrc
 python -m venv /home/vscode/venv
-source /home/vocode/venv/bin/activate && pip3 install -r requirements.txt
+source /home/vocode/venv/bin/activate
 
 #su $USER -c "pip3 --disable-pip-version-check --no-cache-dir install -r requirements.txt"
