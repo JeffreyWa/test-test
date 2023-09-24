9f99c6c6618992c02bf8dd184324ee1b4d1e617e
JeffreyWa <wang_jun_zhan@yahoo.com>
2023-09-24 01:28:12 +0000

    upate

--- setup.sh
+++ setup.sh
@@ -5,9 +5,10 @@ set -x
 #USER="vscode"
 #VENV_PATH="/home/${USER}/venv"
 #su $USER -c "/usr/bin/python3 -m venv /home/${USER}/venv"
+echo 'source /home/vscode/venv/bin/activate' >> ~/.bashrc
 python -m venv /home/vscode/venv
 source /home/vocode/venv/bin/activate
 pip3 --disable-pip-version-check --no-cache-dir install -r requirements.txt
 #append it to bash so every shell launches with it 
-echo 'source /home/vscode/venv/bin/activate' >> ~/.bashrc
+
 #su $USER -c "pip3 --disable-pip-version-check --no-cache-dir install -r requirements.txt"
